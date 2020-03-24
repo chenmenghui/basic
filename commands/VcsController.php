eea2c9889c5063daf3ec26cbaab3b4aa4907a9b8
Valen <chenmenghui_008@163.com>
2020-03-24 17:41:59 +0800

    k

---
+++ commands/VcsController.php
@@ -0,0 +1,66 @@
+<?php
+
+
+namespace app\commands;
+
+
+use yii\console\Controller;
+use yii\console\ExitCode;
+use yii\console\widgets\Table;
+
+
+/**
+ * This controller for vcs(svn) of company
+ * Class VcsController
+ * @package app\commands
+ */
+class VcsController extends Controller
+{
+    public function actionIndex()
+    {
+        echo Table::widget([
+            'headers' => ['Project', 'Status', 'Participant'],
+            'rows'    => [
+                ['Yii', 'OK', '@samdark'],
+                ['Yii', 'OK', '@cebe'],
+            ],
+        ]);
+        return ExitCode::OK;
+    }
+
+    public function actionInsert()
+    {
+        $filePath = [
+            __DIR__ . '/../temp/rc_svn_committed_record.txt',
+        ];
+        foreach ($filePath as $item) {
+            $this->exportFile($item);
+        }
+    }
+
+    private function exportFile($filePath)
+    {
+        $result = [];
+        $pattern = [
+            'revision' => '/Revision: (?<value>\d*)/',
+            'author'   => '/Author: (?<value>\S*)/',
+            'date'     => '/Date: (?<value>\S*)/',
+            'message'  => '/Message:\n(?<value>(.|(\n\r)|\r|\n)*?)----/',
+            'path'     => '/(?<value>(Added|Modified|Deleted) : (.*((\n\r)|\r|\n)+)*?)\n/',
+        ];
+        $content = file_get_contents($filePath);
+        $n = 0;
+        foreach ($pattern as $key => $item) {
+            preg_match_all($item, $content, $match, PREG_SET_ORDER);
+
+            foreach ($match as $matchItem) {
+                $result[$n][$key] = $matchItem['value'];
+                $n++;
+            }
+            unset($match);
+            $n = 0;
+        }
+
+        file_put_contents('test.php', var_export($result, 1));
+    }
+}
